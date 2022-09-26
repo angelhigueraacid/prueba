9121abaa7a3300899a2c7ee2527ca82e9ad51be8
Ángel Higuera Vaquero <angelhiguera@acidtango.com>
2022-09-26 09:28:18 +0200

    feat: add demo application tests

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,28 +1,28 @@
 package com.example.demo;
 
-import java.util.Date;
+import java.text.SimpleDateFormat;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.example.demo.api.prices.dtos.FindProductPriceDto;
 import com.example.demo.prices.domain.Currency;
 import com.example.demo.prices.domain.ProductPrice;
 import com.example.demo.prices.domain.ProductPriceRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import com.fasterxml.jackson.databind.SerializationFeature;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class DemoApplicationTests {
 
 	@Autowired
@@ -31,30 +31,108 @@ class DemoApplicationTests {
 	@Autowired
 	private ProductPriceRepository productPriceRepository;
 
+	@BeforeAll
+	private void setup() throws Exception {
+		productPriceRepository.save(new ProductPrice(1L, 35455L, 1L, 0,
+				35.50, Currency.EUR,
+				new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse("2020-06-14 00:00:00"),
+				new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse("2020-12-31 23:59:59")));
+		productPriceRepository.save(new ProductPrice(1L, 35455L, 2L, 1,
+				25.45, Currency.EUR,
+				new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse("2020-06-14 15:00:00"),
+				new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse("2020-06-14 18:30:00")));
+		productPriceRepository.save(new ProductPrice(1L, 35455L, 3L, 1,
+				30.50, Currency.EUR,
+				new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse("2020-06-15 00:00:00"),
+				new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse("2020-06-15 11:00:00")));
+		productPriceRepository.save(new ProductPrice(1L, 35455L, 4L, 1,
+				38.95, Currency.EUR,
+				new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse("2020-06-15 16:00:00"),
+				new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse("2020-12-31 23:59:59")));
+
+	}
+
 	@Test
-	void contextLoads() {
+	public void givenProductPrices_whenGetProductPriceOnDay14At10_thenStatus200()
+			throws Exception {
+
+		FindProductPriceDto dto = createFindProductPriceDto("2020-06-14 10:00:00");
+
+		mvc.perform(get("/prices")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(asJsonString(dto)))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.finalPrice").value(35.50));
+
 	}
 
 	@Test
-	public void givenProductPrices_whenGetProductPrice_thenStatus200() throws Exception {
-		createProductPrices();
+	public void givenProductPrices_whenGetProductPriceOnDay14At16_thenStatus200()
+			throws Exception {
+		FindProductPriceDto dto = createFindProductPriceDto("2020-06-14 16:00:00");
 
-		FindProductPriceDto dto = new FindProductPriceDto();
-		dto.setBrandId(1L);
-		dto.setInDate(new Date());
-		dto.setProductId(1L);
-
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
-		ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
-		String sendJson = ow.writeValueAsString(dto);
-
-		mvc.perform(get("/prices").contentType(MediaType.APPLICATION_JSON).content(sendJson))
-				.andExpect(status().isOk());
+		mvc.perform(get("/prices")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(asJsonString(dto)))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.finalPrice").value(25.45));
 
 	}
 
-	private void createProductPrices() {
-		productPriceRepository.save(new ProductPrice(1L, 1L, 1L, 1, 35.45, Currency.EUR, new Date(), new Date()));
+	@Test
+	public void givenProductPrices_whenGetProductPriceOnDay14At21_thenStatus200()
+			throws Exception {
+		FindProductPriceDto dto = createFindProductPriceDto("2020-06-14 21:00:00");
+
+		mvc.perform(get("/prices")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(asJsonString(dto)))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.finalPrice").value(35.50));
+
 	}
+
+	@Test
+	public void givenProductPrices_whenGetProductPriceOnDay15At10_thenStatus200() throws Exception {
+
+		FindProductPriceDto dto = createFindProductPriceDto("2020-06-15 10:00:00");
+
+		mvc.perform(get("/prices")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(asJsonString(dto)))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.finalPrice").value(30.50));
+
+	}
+
+	@Test
+	public void givenProductPrices_whenGetProductPriceOnDay15At21_thenStatus200() throws Exception {
+		FindProductPriceDto dto = createFindProductPriceDto("2020-06-15 21:00:00");
+
+		mvc.perform(get("/prices")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(asJsonString(dto)))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.finalPrice").value(38.95));
+
+	}
+
+	public FindProductPriceDto createFindProductPriceDto(String date) throws Exception {
+
+		FindProductPriceDto findProductPriceDto = new FindProductPriceDto();
+		findProductPriceDto.setBrandId(1L);
+		findProductPriceDto.setInDate(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse(date));
+		findProductPriceDto.setProductId(35455L);
+
+		return findProductPriceDto;
+	}
+
+	public static String asJsonString(final Object obj) {
+		try {
+			return new ObjectMapper().writeValueAsString(obj);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
 }
